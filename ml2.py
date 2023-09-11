51d665d10c1bd28b42b3f5fcc54c4c36c33cd542
bvenkatabalaji297 <144209578+bvenkatabalaji297@users.noreply.github.com>
2023-09-11 21:18:32 +0530

    Machine Learning task 2

---
+++ ml2.py
@@ -0,0 +1,44 @@
+# Import necessary libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the wine quality dataset (you can download it from sources like UCI Machine Learning Repository)
+data = pd.read_csv('wine_quality.csv')  # Replace with the path to your dataset
+
+# Explore and preprocess the data (e.g., handle missing values, encode categorical variables)
+
+# Select relevant features (attributes)
+features = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
+            'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
+
+X = data[features]
+y = data['quality']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create and train the linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the testing set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Squared Error: {mse}")
+print(f"Root Mean Squared Error: {rmse}")
+print(f"R-squared: {r2}")
+
+# Visualize the results if needed (e.g., scatter plot of actual vs. predicted wine quality)
+
+# Make predictions on new data if needed
+
+# Save or deploy the trained model for future use
